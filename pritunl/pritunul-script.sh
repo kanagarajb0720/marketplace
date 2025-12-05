2992f59ac2d0eded97ded672d156fef5f8141b6e
kanagaraj <kanagaraj.b@stackbill.com>
2025-12-05 16:38:15 +0530

    save

--- pritunl/pritunul-script.sh
+++ pritunl/pritunul-script.sh
@@ -2,8 +2,8 @@
 
 mkdir -p {/usr/local/src/pritunl}
 
-wget -O /usr/local/src/pritunl/cleanup.sh https://raw.githubusercontent.com/stackbill/marketplace/refs/heads/main/pritunl/pritunul-cleanup.sh
+wget -O /usr/local/src/pritunl/cleanup.sh https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/pritunl/pritunul-cleanup.sh
 
 chmod +x /usr/local/src/pritunl/cleanup.sh
 
-cd /usr/local/src/harbor/ && wget https://raw.githubusercontent.com/stackbill/marketplace/refs/heads/main/pritunl/pritunul-install.yaml
+cd /usr/local/src/pritunl/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/pritunl/pritunul-install.yaml
