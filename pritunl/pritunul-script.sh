c635713e1f57e8ae97aa9a4f78ce190d22a59e31
kanagarajb0720 <kanagaraj.b@stackbill.com>
2025-12-10 14:53:16 +0530

    Update pritunul-script.sh

--- pritunl/pritunul-script.sh
+++ pritunl/pritunul-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p {/usr/local/src/pritunl}
+mkdir -p /usr/local/src/pritunl
 
 wget -O /usr/local/src/pritunl/cleanup.sh https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/pritunl/pritunul-cleanup.sh
 
