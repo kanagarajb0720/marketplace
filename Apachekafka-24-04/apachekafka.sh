3dd2292912960f5da687b4aa70646ccd688d7a86
kanagaraj <kanagaraj.b@stackbill.com>
2026-06-13 20:40:11 +0530

    apachekhafa

--- Apachekafka-24-04/apachekafka.sh
+++ Apachekafka-24-04/apachekafka.sh
@@ -2,12 +2,12 @@
 
 mkdir -p {/usr/local/src/apachekafka-24-04/opt/cloudstack/,/usr/local/src/apachekafka-24-04/,/usr/local/src/apachekafka-24-04/opt/kafka/config/,/usr/local/src/apachekafka-24-04/etc/systemd/system/}
 
-cd /usr/local/src/apachekafka-24-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/Apachekafka-24-04/apachekafka-cleanup.sh
+cd /usr/local/src/apachekafka-24-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/refs/heads/main/Apachekafka-24-04/apachekafka-cleanup.sh
 
-cd /usr/local/src/apachekafka-24-04/etc/systemd/system/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/Apachekafka-24-04/zookeeper.service
+cd /usr/local/src/apachekafka-24-04/etc/systemd/system/ && wget https://raw.githubusercontent.com/stackbill/marketplace/refs/heads/main/Apachekafka-24-04/zookeeper.service
 
-cd /usr/local/src/apachekafka-24-04/etc/systemd/system/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/Apachekafka-24-04/kafka.service
+cd /usr/local/src/apachekafka-24-04/etc/systemd/system/ && wget https://raw.githubusercontent.com/stackbill/marketplace/refs/heads/main/Apachekafka-24-04/kafka.service
 
-cd /usr/local/src/apachekafka-24-04/opt/kafka/config/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/Apachekafka-24-04/server.properties
+cd /usr/local/src/apachekafka-24-04/opt/kafka/config/ && wget https://raw.githubusercontent.com/stackbill/marketplace/refs/heads/main/Apachekafka-24-04/server.properties
 
-cd /usr/local/src/apachekafka-24-04/ && wget https://raw.githubusercontent.com/kanagarajb0720/marketplace/refs/heads/main/Apachekafka-24-04/apachekafka.yaml
+cd /usr/local/src/apachekafka-24-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/refs/heads/main/Apachekafka-24-04/apachekafka.yaml
